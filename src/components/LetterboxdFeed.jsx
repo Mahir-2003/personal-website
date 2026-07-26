7846e73ac102005970ae555ecf663bd040f50b5b
Mahir Khan <mahirbd.khan@gmail.com>
2026-07-26 15:42:15 -0700

    update LetterboxdFeed to display one additional movie and adjust grid layout

--- src/components/LetterboxdFeed.jsx
+++ src/components/LetterboxdFeed.jsx
@@ -92,7 +92,7 @@ const LetterboxdFeed = () => {
                 </div>
 
                 {/* Movie grid */}
-                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3">
+                <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-3">
                     {movies.map((movie, index) => (
                         <a
                             key={index}
